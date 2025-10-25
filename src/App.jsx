962dbb66307e4e70e9a618262f9ddcd1381e45f2
Harishankar <charishankar30@gmail.com>
2025-10-25 14:53:41 +0530

    cleaned file

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,6 @@ import React, { useState, useEffect } from "react";
 
 function App() {
 
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
-  }, []);
-
-  if (loading) return <Loader/>;
-
   return <HomePage />;
 }
 
