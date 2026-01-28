10b2d74a8d415a50d117b75332746da3bd0f59a0
Shamil Niswan <niswannassar@gmail.com>
2026-01-28 17:44:40 +0530

    jhgkg

--- Backend/clear_users.sql
+++
@@ -1,5 +0,0 @@
--- First, delete any existing admin users
-DELETE FROM users WHERE email = 'admin@iiitkottayam.ac.in';
-
--- This will be done via API instead
--- The password needs to be hashed by bcrypt during user creation
--- Backend/delete_admin.sql
+++
@@ -1,2 +0,0 @@
--- Run this in phpMyAdmin SQL tab to delete existing admin user
-DELETE FROM users WHERE email = 'admin@iiitkottayam.ac.in';
